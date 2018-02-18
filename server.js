646998b29e6d82a72a568c2df52219bba9227187
aidanclemente <aidanclemente@gmail.com>
2018-02-18 15:57:31 -0500

    set up html

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+ 
+app.get('/', function (req, res) {
+  res.send('Hello Aidan')
+})
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.listen(PORT, function() {
+    console.log("App listening on Port: " + PORT);
+});
