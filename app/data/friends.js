dd9693d8ae97179dc8f61e2302de5c0a55f10372
aidanclemente <aidanclemente@gmail.com>
2018-02-18 22:47:36 -0500

    started on routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,22 @@
 // You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
-
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
+[
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+]
 // Determine the user's most compatible friend using the following as a guide:
 
 // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
--- server.js
+++ server.js
@@ -4,17 +4,16 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
- 
-app.get('/', function (req, res) {
-  res.send('Hello Aidan')
-})
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+// Includes the routes that were exported and passing app to use express
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on Port: " + PORT);
